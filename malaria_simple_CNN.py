195a23f14912df7f0871c60ab61905773879766f
Alessandro Crimi <alecrimi@alice.it>
2020-04-11 23:20:53 +0200

    Update and rename malaria_run.py to malaria_simple_CNN.py

--- malaria_simple_CNN.py
+++ malaria_simple_CNN.py
@@ -8,6 +8,9 @@ from collections import Counter
 import cv2  #OpenCV to load images
 from concurrent import futures
 import threading
+import tensorflow as tf
+
+import datetime # For log
 
 base_dir = os.path.join('./cell_images')
 infected_dir = os.path.join(base_dir,'Parasitized')
@@ -19,7 +22,13 @@ print(len(infected_files), len(healthy_files))
 
 np.random.seed(42)
 IMG_DIMS = (125, 125)
+BATCH_SIZE = 64
+NUM_CLASSES = 2
+EPOCHS = 25
+INPUT_SHAPE = (125, 125, 3)
 
+
+######################### PREPARING DATA #######################################
 # Load pictures in parallel
 def get_img_data_parallel(idx, img, total_imgs):
     if idx % 5000 == 0 or idx == (total_imgs - 1):
@@ -73,8 +82,9 @@ test_data_map = ex.map(get_img_data_parallel,
                         [record[2] for record in test_data_inp])
 test_data = np.array(list(test_data_map))
 
+#If you want to visualize some loaded cells use this:
+'''
 import matplotlib.pyplot as plt
-
 plt.figure(1 , figsize = (8 , 8))
 n = 0
 for i in range(16):
@@ -85,3 +95,72 @@ for i in range(16):
     plt.imshow(train_data[r[0]]/255.)
     plt.title('{}'.format(train_labels[r[0]]))
     plt.xticks([]) , plt.yticks([])  
+plt.show()
+'''
+
+train_imgs_scaled = train_data / 255.
+val_imgs_scaled = val_data / 255.
+
+# encode text category labels into numerical lables
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+le.fit(train_labels)
+train_labels_enc = le.transform(train_labels)
+val_labels_enc = le.transform(val_labels)
+#print(train_labels[:6], train_labels_enc[:6])
+
+#################################### CNN MODEL ##########################
+tf.random.set_seed(42)
+
+inp = tf.keras.layers.Input(shape=INPUT_SHAPE)
+
+conv1 = tf.keras.layers.Conv2D(32, kernel_size=(3, 3), 
+                               activation='relu', padding='same')(inp)
+pool1 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv1)
+conv2 = tf.keras.layers.Conv2D(64, kernel_size=(3, 3), 
+                               activation='relu', padding='same')(pool1)
+pool2 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv2)
+conv3 = tf.keras.layers.Conv2D(128, kernel_size=(3, 3), 
+                               activation='relu', padding='same')(pool2)
+pool3 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv3)
+
+flat = tf.keras.layers.Flatten()(pool3)
+
+hidden1 = tf.keras.layers.Dense(512, activation='relu')(flat)
+drop1 = tf.keras.layers.Dropout(rate=0.3)(hidden1)
+hidden2 = tf.keras.layers.Dense(512, activation='relu')(drop1)
+drop2 = tf.keras.layers.Dropout(rate=0.3)(hidden2)
+
+out = tf.keras.layers.Dense(1, activation='sigmoid')(drop2)
+
+model = tf.keras.Model(inputs=inp, outputs=out)
+model.compile(optimizer='adam',
+                loss='binary_crossentropy',
+                metrics=['accuracy'])
+#model.summary()
+
+################################ TRAINING ###############################
+logdir = os.path.join('./', 
+                      datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1)
+reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5,
+                              patience=2, min_lr=0.000001)
+callbacks = [reduce_lr, tensorboard_callback]
+
+history = model.fit(x=train_imgs_scaled, y=train_labels_enc, 
+                    batch_size=BATCH_SIZE,
+                    epochs=EPOCHS, 
+                    validation_data=(val_imgs_scaled, val_labels_enc), 
+                    callbacks=callbacks,
+                    verbose=1)
+
+# Save the model to be used for classification
+model.save('basic_cnn.h5')
+
+
+
+
+
+
+
